77de517f9c6e9642745b52e37f1d466f29d127e4
Michael <xywang02@gmail.com>
2021-11-28 16:10:46 -0500

    bug: changed __init__.py for packages

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
+from .animation_3d import AnimationJoint
+from .animation_3d import AnimationLimb
+from .animation_3d import Animation3D
+from .utilities import view_matrix_3d
+
 name = "Animation3D"
 __all__ = [
     "animation_3d",
--- src/animation_3d/__init__.py
+++ src/animation_3d/__init__.py
@@ -1,3 +1,7 @@
+import Animation3D
+import AnimationJoint
+import AnimationLimb
+
 name = "animation3d"
 __all__ = [
     "AnimationJoint",
