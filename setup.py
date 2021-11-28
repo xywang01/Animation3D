dc98bccffe6f0e528746d112e6369b9f33d1f76c
Michael <xywang02@gmail.com>
2021-11-28 16:13:23 -0500

    bug: edited long description

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="animation3d",
     version="0.0.1",
     author="WXY",
     author_email="",
     description="A package that uses Matplotlib to create 3D/2D animations of human movement data.",
-    long_description=long_description,
+    long_description="This is a long description",
     long_description_content_type="text/markdown",
     url="https://github.com/xywang01/Animation3D",
     packages=setuptools.find_packages(),
