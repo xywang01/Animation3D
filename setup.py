7838d9526822dec3256ce1f006a923dcd642b2ed
Michael <xywang02@gmail.com>
2021-11-28 16:01:32 -0500

    changed metadata name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="animation_3d",
+    name="animation3d",
     version="0.0.1",
     author="WXY",
     author_email="",
